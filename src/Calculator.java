cb8fda3b5c95281295dd475106d9ae8b4fc12cf1
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2023-12-22 22:54:57 +0100

    Division by zero was handled

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,27 +1,33 @@
 public class Calculator {
 
-    public double sum(int n, int m, int a, int b) {
+    public static void main(String[] args) {
+        try {
+            double result = sum(7, 10, 1, 1);
+            System.out.println("Result: " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static double sum(int n, int m, int a, int b) {
         if (n <= a || m <= b) {
             throw new IllegalArgumentException("Invalid range: n should be greater than a, and m should be greater than b.");
+        }
+
+        if ((a <= 0 && n >= 0) || (b <= 0 && m >= 0)) {
+            throw new IllegalArgumentException("It's impossible to divide by zero. Check values: a=" + a + ", b=" + b + ", n=" + n + ", m=" + m);
         }
 
         double result2 = 0;
         for (int i = a; i <= n; i++) {
             double result1 = 0;
             for (int j = b; j <= m; j++) {
-                if (j == 0) {
-                    throw new IllegalArgumentException("Division by zero is not allowed.");
+                if (i != 0 && j != 0) {
+                    result1 += ((double) (i % j)) / i;
                 }
-                result1 += ((double) (i % j)) / (i + 374);
             }
             result2 += result1;
         }
         return result2;
-    }
-
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        double result = calculator.sum(7, 10, 1, 1);
-        System.out.println("Result: " + result);
     }
 }
